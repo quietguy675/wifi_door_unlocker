55f9cfbf9b1a00de219c98da738bfbd07f10c8bd
Matt From <matfrom@gmail.com>
2017-01-13 04:47:07 -0600

    Added code

---
+++ wifi_door_unlocker/wifi_door_unlocker.ino
@@ -0,0 +1,108 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Servo.h>
+#define SERVO_OPEN_ANGLE 0
+#define SERVO_CLOSE_ANGLE 180
+#define SERVO_POWER_PIN 0
+
+const char* ssid = "The WLAN Before Time";
+const char* password = "khk1924delta";
+
+ESP8266WebServer server(80);
+Servo myservo;
+
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!");
+}
+
+void setDoor(bool open_close) {
+  if (open_close == 1) {
+    myservo.write(SERVO_OPEN_ANGLE);
+  } else {
+    myservo.write(SERVO_CLOSE_ANGLE);
+  }
+  delay(5);
+  digitalWrite(SERVO_POWER_PIN, HIGH);
+  delay(500);
+  digitalWrite(SERVO_POWER_PIN, LOW);
+}
+
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void handleDoor(){
+  String message = "";
+  if (server.argName(0) == "door" && server.arg(0) == "on"){
+    setDoor(1);
+    server.send(200, "text/plain", "Door Should Be Open");
+    return;
+  } else if (server.argName(0) == "door" && server.arg(0) == "off"){
+    setDoor(0);
+    server.send(200, "text/plain", "Door Should Be Closed");
+    return;
+  } else {
+    message += "Invalid arguments:\n";
+    for (uint8_t i=0; i<server.args(); i++){
+      message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+    server.send(404, "text/plain", message);
+  }
+  return;
+}
+
+void setup(void){
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  pinMode(SERVO_POWER_PIN, OUTPUT);
+  digitalWrite(SERVO_POWER_PIN, LOW);
+  setDoor(0);
+  myservo.attach(2);
+  myservo.write(10);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/wank", handleDoor);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
